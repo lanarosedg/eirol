4ed35bf40a8f0c070e06adf7385c9eff670f768d
Lana Rose <lanarosedeguzman@gmail.com>
2025-07-03 20:45:18 +0800

    Minor changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,10 @@ import container1Photo from './assets/firstContainerPhoto.jpg';
 import logo from './assets/eirolLogo.png';
 import products1 from './assets/Products1.png';
 import hand from './assets/hand.png';
+import soap from './assets/soap.png';
+import leftArrow from './assets/leftArrow.png';
+import rightArrow from './assets/rightArrow.png';
+
 function Home () {
 return (
     <>
@@ -33,28 +37,60 @@ return (
             Partner with us to transform your
         </p>
         <p className="vision">
-            <strong>vision </strong>
-            into
-            <strong> reality</strong>
+            vision
+            <strong> into </strong>
+            reality
         </p>
         <a className="partnerBrands" href="">
             Partner brands {'>'}
         </a>
     </div>
     <div className="parentContainer4">
-        <div className="firstpc3">
-            <div className="intro">
-                <p className="introText">
+        <div className="intro">
                 At Eirol Cosmetics, we specialize in premium 
-                cosmetic manufacturing, offering innovative, high-quality products to elevate your brand. 
-                </p>
-            </div>
+                cosmetic manufacturing, <br />offering innovative, high-quality products to elevate your brand. 
         </div>
+        
         <img 
             src={hand}
             alt="" 
             className="hand" 
         />
+
+        <div className="threeWords">
+            uniqueness. <br />
+            quality. <br />
+            authenticity.
+        </div>
+    </div>
+    <div className="parentContainer5">
+        <img 
+            src={soap}
+            alt="" 
+            className="soap" 
+        />
+
+        <img 
+            src={leftArrow} 
+            alt="" 
+            className="leftArrow" 
+        />
+
+        <img 
+            src={rightArrow} 
+            alt="" 
+            className="rightArrow" 
+        />
+        <p className="earth">
+            earth-made
+        </p>
+        <p className="skinLoved">
+            skin-loved
+        </p>
+    </div>
+    <div className="parentContainer6">
+        “Crafted Clean. Naturally Yours—Partner with Our <span className='pink'>Expert Chemist</span> to <br />
+        <span className='pink'>Formulate Your Signature Soap.</span>”
     </div>
     </>
 )
