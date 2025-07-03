5716703d882305aae58845fc4f02c9330b67eed0
Lana Rose <lanarosedeguzman@gmail.com>
2025-07-03 17:59:06 +0800

    Add add

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,7 @@
 import container1Photo from './assets/firstContainerPhoto.jpg';
 import logo from './assets/eirolLogo.png';
+import products1 from './assets/Products1.png';
+import hand from './assets/hand.png';
 function Home () {
 return (
     <>
@@ -19,8 +21,40 @@ return (
             Contact Us
         </button>
     </div>
+    <div className="parentContainer3">
+        <img 
+         src={products1}
+         alt="" 
+         className="products1" 
+        />
+    </div>
     <div className="parentContainer2">
-        
+        <p className="partner">
+            Partner with us to transform your
+        </p>
+        <p className="vision">
+            <strong>vision </strong>
+            into
+            <strong> reality</strong>
+        </p>
+        <a className="partnerBrands" href="">
+            Partner brands {'>'}
+        </a>
+    </div>
+    <div className="parentContainer4">
+        <div className="firstpc3">
+            <div className="intro">
+                <p className="introText">
+                At Eirol Cosmetics, we specialize in premium 
+                cosmetic manufacturing, offering innovative, high-quality products to elevate your brand. 
+                </p>
+            </div>
+        </div>
+        <img 
+            src={hand}
+            alt="" 
+            className="hand" 
+        />
     </div>
     </>
 )
